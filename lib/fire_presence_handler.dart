d33a94cfef243eed74557ea2d5d0a1363ac828e5
Youssef Elshiaty N. <155442184+youssefelshiaty@users.noreply.github.com>
2025-03-21 16:50:46 +0200

    Refactor

--- lib/fire_presence_handler.dart
+++ lib/fire_presence_handler.dart
@@ -12,18 +12,18 @@ class FirePresenceHandler extends IFirePresenceHandler {
 
   final _hasConnectionController = StreamController<bool>.broadcast();
   StreamSubscription<bool>? _hasConnectionStreamSubscription;
+  StreamSubscription<List<ConnectivityResult>>? _connectivitySubscription;
   bool _isInitialized = false;
 
-  void _init() async {
+  void _init() {
     if (_isInitialized) return;
-    _isInitialized = true;
 
-    Connectivity().onConnectivityChanged.listen((event) {
-      bool isConnected =
-          !event.any((element) => element == ConnectivityResult.none);
+    _connectivitySubscription = Connectivity().onConnectivityChanged.listen((event) {
+      bool isConnected = !event.any((element) => element == ConnectivityResult.none);
       _hasConnectionController.add(isConnected);
     });
 
+    _isInitialized = true;
     debugPrint('FirePresenceHandler: Initialized.');
   }
 
@@ -31,10 +31,8 @@ class FirePresenceHandler extends IFirePresenceHandler {
   Stream<bool> get hasConnectionStream => _hasConnectionController.stream;
 
   Future<bool> _checkConnection() async {
-    List<ConnectivityResult> connectivityResult =
-        await Connectivity().checkConnectivity();
-    return !connectivityResult
-        .any((element) => element == ConnectivityResult.none);
+    List<ConnectivityResult> connectivityResult = await Connectivity().checkConnectivity();
+    return !connectivityResult.any((element) => element == ConnectivityResult.none);
   }
 
   void _listenToConnectionChanges({
@@ -44,8 +42,7 @@ class FirePresenceHandler extends IFirePresenceHandler {
   }) {
     _hasConnectionStreamSubscription?.cancel();
     try {
-      _hasConnectionStreamSubscription =
-          hasConnectionStream.listen((isConnected) {
+      _hasConnectionStreamSubscription = hasConnectionStream.listen((isConnected) {
         debugPrint('ConnectivityHandler: User is online: $isConnected');
         _updatePresence(uid: uid, isOnline: isConnected, onSuccess: onSuccess);
       });
@@ -78,17 +75,23 @@ class FirePresenceHandler extends IFirePresenceHandler {
   void dispose() {
     _hasConnectionController.close();
     _hasConnectionStreamSubscription?.cancel();
+    _connectivitySubscription?.cancel();
   }
 
   @override
-  void connect({
+  Future<void> connect({
     required String uid,
     Function(bool)? onSuccess,
     Function? onError,
-  }) {
-    _init();
-    _listenToConnectionChanges(
-        uid: uid, onSuccess: onSuccess, onError: onError);
+  }) async {
+    if (_isInitialized) {
+      bool isConnected = await _checkConnection();
+      _hasConnectionController.add(isConnected);
+    } else {
+      _init();
+    }
+
+    _listenToConnectionChanges(uid: uid, onSuccess: onSuccess, onError: onError);
   }
 
   @override
@@ -98,13 +101,15 @@ class FirePresenceHandler extends IFirePresenceHandler {
     Function? onError,
   }) {
     _updatePresence(
-        uid: uid,
-        isOnline: false,
-        onSuccess: () {
-          debugPrint('ConnectivityHandler: User forcibly disconnected');
-          _isInitialized = false;
-          onSuccess?.call();
-        },
-        onError: onError);
+      uid: uid,
+      isOnline: false,
+      onSuccess: () {
+        debugPrint('ConnectivityHandler: User forcibly disconnected');
+        _isInitialized = false;
+        _connectivitySubscription?.cancel(); // Stop listening to connectivity changes
+        onSuccess?.call();
+      },
+      onError: onError,
+    );
   }
 }
